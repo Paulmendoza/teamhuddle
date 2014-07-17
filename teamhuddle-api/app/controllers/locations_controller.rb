7c04ace851093547abc85adf17cc24f56919e1c2
Paul Mendoza <pauljoshuam@hotmail.com>
2014-07-17 06:07:09 -0700

    cleanup. location create function

--- teamhuddle-api/app/controllers/locations_controller.rb
+++ teamhuddle-api/app/controllers/locations_controller.rb
@@ -1,26 +1,35 @@
 class LocationsController < ApplicationController
 
   # GET /locations.json
+  # GET /locations.xml
+  # GET /locations
   def index
-    @locations = Location.all
+    @locations = Location.all.select("id, name, lat, long, address")
 
     respond_to do |format|
+      format.html { render json: @locations }
       format.json { render json: @locations }
       format.xml { render xml: @locations }
     end
+
   end
 
   # POST /locations
+  # example:
+  # { "location": { "lat": 1, "long": 2, "name": "my house2" } }
   def create
     @location = Location.new(location_params)
-
-    @location.save
+    
+    unless params[:name].blank?
+      @location.save
+    end
+  
     redirect_to @location
   end
 
   private
   def location_params
-    params.require(:location).permit(:lat, :long, :name, :address)
+    params.require(:location).permit(:name, :lat, :long, :address)
   end
 
 end
