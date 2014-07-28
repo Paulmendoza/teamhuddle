520c31422c9947b10e2a6f7dad1492d8237d5fc8
akossebestyen <akos_sebestyen@hotmail.com>
2014-07-28 01:28:37 -0700

    added a hint of styling and locations works

---
+++ teamhuddle-api/config/initializers/assets.rb
@@ -0,0 +1,5 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+Rails.application.config.assets.precompile += %w( style.css )
