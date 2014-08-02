70211f0c2fe4d6d2a6a4c0bcc0adae07949df99c
akossebestyen <akos_sebestyen@hotmail.com>
2014-08-02 12:14:48 -0700

    minor model updates, todo updated

--- teamhuddle-api/app/models/event.rb
+++ teamhuddle-api/app/models/event.rb
@@ -2,6 +2,7 @@ class Event < ActiveRecord::Base
   
   has_one :location
   has_one :organization
+  has_one :sport_event, :dependent => :delete
 
   validates :name, :presence => true, :uniqueness => true
   validates :location_id, :presence => true
