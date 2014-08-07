ab38d07adf8781d1396c7c5a46f69fda96ebb97d
Paul Mendoza <pauljoshuam@hotmail.com>
2014-08-06 19:20:16 -0700

    users table got moved from other branch somehow. using it in this branch

--- teamhuddle-api/db/schema.rb
+++ teamhuddle-api/db/schema.rb
@@ -88,14 +88,21 @@ ActiveRecord::Schema.define(version: 20140803023605) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "username"
-    t.string   "salthashpass"
-    t.string   "salt"
-    t.string   "first_name"
-    t.string   "locale"
-    t.string   "email"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
