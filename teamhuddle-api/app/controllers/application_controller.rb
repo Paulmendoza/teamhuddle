b2fec0d54e018c737818b31cc28717094f8d57cb
akossebestyen <akos_sebestyen@hotmail.com>
2014-09-18 19:21:23 -0700

    CORS fix for webserver

--- teamhuddle-api/app/controllers/application_controller.rb
+++ teamhuddle-api/app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   include ActionController::MimeResponds
+  
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
 
   rescue_from(ActionController::ParameterMissing) do |parameter_missing_exception|
     error = {}
@@ -20,5 +23,20 @@ class ApplicationController < ActionController::Base
       format.json { render json: response, status: :not_found }
     end
   end
-
+  
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+  
+  def cors_preflight_check
+    if request.method == :options
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+      headers['Access-Control-Request-Method'] = '*'
+      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    end
+  end
 end
