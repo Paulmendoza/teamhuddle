92f136427ca897eef9172de5ee2a611b09ed046a
akossebestyen <akos_sebestyen@hotmail.com>
2014-10-08 18:29:22 -0700

    TH-31 removed CORS as it is no longer needed

--- teamhuddle-api/app/controllers/application_controller.rb
+++ teamhuddle-api/app/controllers/application_controller.rb
@@ -2,9 +2,6 @@ class ApplicationController < ActionController::Base
   include ActionController::MimeResponds
   
   layout 'admin'
-  
-  before_filter :cors_preflight_check
-  after_filter :cors_set_access_control_headers
 
   rescue_from(ActionController::ParameterMissing) do |parameter_missing_exception|
     error = {}
@@ -23,22 +20,6 @@ class ApplicationController < ActionController::Base
     respond_to do |format|
       format.html { render json: response, status: :not_found }
       format.json { render json: response, status: :not_found }
-    end
-  end
-  
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = '*'
-    headers['Access-Control-Max-Age'] = "1728000"
-  end
-  
-  def cors_preflight_check
-    if request.method == :options
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
-      headers['Access-Control-Request-Method'] = '*'
-      headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     end
   end
 end
--- teamhuddle-api/config/application.rb
+++ teamhuddle-api/config/application.rb
@@ -9,14 +9,7 @@ Bundler.require(*Rails.groups)
 module TeamhuddleApi
   class Application < Rails::Application
     config.assets.enabled = false
-    config.middleware.use Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
-      end
-    end
-    
-    config.middleware.use Rack::MethodOverride
+   
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
