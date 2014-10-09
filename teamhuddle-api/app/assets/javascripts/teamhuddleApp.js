1cb8585709b64f13e7a05cd9ebfcd280701829c3
akossebestyen <akos_sebestyen@hotmail.com>
2014-10-08 18:10:00 -0700

    TH-12 filtering by day of week works

--- teamhuddle-api/app/assets/javascripts/teamhuddleApp.js
+++ teamhuddle-api/app/assets/javascripts/teamhuddleApp.js
@@ -6,23 +6,24 @@ app.controller('dropins', ['$scope', '$filter', 'Dropins', function ($scope, $fi
         var orderBy = $filter('orderBy');
         $scope.dropins = [
             {
-                datetime_start: { time: ''} ,
-                datetime_end: { time: ''} ,
-                event: { name: ''},
-                location: { name: ''}
+                datetime_start: {time: ''},
+                datetime_end: {time: ''},
+                event: {name: ''},
+                location: {name: ''}
             }
         ];
-        
+
         $scope.remove = function (item) {
             var index = $scope.dropins.indexOf(item);
             $scope.dropins.splice(index, 1);
         };
 
         // use a deferred promise from the Dropins service to populate the scope
-        $scope.refresh = function () {
+        $scope.refreshDropins = function () {
+            $scope.weekdaySelect = "All";
             Dropins.get().then(
                     function (dropins) {
-                        $scope.dropins = orderBy(dropins, 'datetime_start');
+                        $scope.dropins = orderBy(dropins, 'datetime_start.time');
                     },
                     function (reason) {
                         alert('Failed: ' + reason);
@@ -33,25 +34,43 @@ app.controller('dropins', ['$scope', '$filter', 'Dropins', function ($scope, $fi
         $scope.order = function (predicate, reverse) {
             $scope.dropins = orderBy($scope.dropins, predicate, reverse);
         };
-        
-        $scope.refresh();
 
-        $scope.centerDropin = function(dropin){
+        $scope.refreshDropins();
+
+        $scope.centerDropin = function (dropin) {
             $scope.map.panTo(new google.maps.LatLng(dropin.location.lat, dropin.location.long));
             $scope.map.setZoom(11);
         };
-        
-        $scope.$watch($scope.weekdaySelect)
-        console.log();
+
+
+
+        $scope.filterDays = function () {
+            if($scope.weekdaySelect === "All"){
+                $scope.refreshDropins();
+            }
+            else{
+               Dropins.get().then(
+                    function (dropins) {
+                        $scope.dropins = $filter('weekday')(dropins, $scope.weekdaySelect);
+                    },
+                    function (reason) {
+                        alert('Failed: ' + reason);
+                    }); 
+            }
+            
+        }
+
     }]);
 
-app.filter('weekdayFilter', function() {
-    return function(items) {
-        var retn = [];
 
-        angular.forEach(items, function(item){
-            if((new Date().getFullYear()) - (new Date(+item.InfoDetails[0].FromDt.substring(6, item.InfoDetails[0].FromDt.length - 2)).getFullYear()) >= 3){
-              retn.push(item); 
+app.filter('weekday', function ($filter) {
+    return function (sport_events, day) {
+        var retn = [];
+        
+        angular.forEach(sport_events, function (sport_event) {
+            if ($filter('date')(sport_event.datetime_start.time, 'EEEE') === day) {
+                
+                retn.push(sport_event);
             }
         });
 
@@ -60,3 +79,5 @@ app.filter('weekdayFilter', function() {
         return retn;
     };
 });
+
+
