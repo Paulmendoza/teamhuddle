e734339071bed86ad62f65724fbc5b8e2f0eee75
Zamina Sunderji <zaminasunderji@gmail.com>
2014-11-09 00:56:27 -0800

    weird wording in routes.rb removed

--- teamhuddle-api/config/routes.rb
+++ teamhuddle-api/config/routes.rb
@@ -19,10 +19,7 @@ Rails.application.routes.draw do
   
   resources :static_pages 
 
-  resources :users do only: :new :create
-  end
-  
-  
+  resources :users
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
