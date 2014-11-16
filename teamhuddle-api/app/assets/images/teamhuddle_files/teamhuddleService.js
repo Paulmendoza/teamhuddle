493a758f0274c698c5c04b250f71d2a8e12d15e6
Zamina Sunderji <zaminasunderji@gmail.com>
2014-11-15 17:08:41 -0800

    mobile dropdown expands to full width of screen

---
+++ teamhuddle-api/app/assets/images/teamhuddle_files/teamhuddleService.js
@@ -0,0 +1,37 @@
+var app = angular.module('app', ['ngResource', 'ngMap', 'ngAnimate']);
+
+
+// the dropins service that is expandable with new functions easily
+app.service('Dropins', function($http, $q){
+    
+    var baseUrl = '/api/v1/api_dropins.json?';
+            
+            
+    baseUrl += 'from=' + moment().startOf('week').format();        
+    baseUrl += '&to=' + moment().endOf('week').format(); 
+    
+    
+    // default get
+    this.get = function(){
+        return this.fetch(baseUrl);
+    };
+    
+    this.getBySport = function(sport){
+        var url = baseUrl + '&sport=' + sport;
+        return this.fetch(url);
+    };
+    
+    // 
+    this.fetch = function(builtUrl) {
+        var deferred = $q.defer();
+        
+        $http.get(builtUrl).success(function(data){
+            deferred.resolve(data.dropins);
+        }).error(function(data) {
+            deferred.reject(data.dropins);
+        });
+        
+        return deferred.promise;
+    };
+    
+});
