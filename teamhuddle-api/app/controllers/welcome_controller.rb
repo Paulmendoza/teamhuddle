ad39b00f1ad1dbd2d9df3a1c8e448b6d28d973bc
Akos Sebestyen <akos_sebestyen@hotmail.com>
2014-11-28 22:14:33 -0800

    email list ordering fix

--- teamhuddle-api/app/controllers/welcome_controller.rb
+++ teamhuddle-api/app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   before_action :authenticate_admin!  
  
   def index
-    @users = User.all
+    @users = User.all.order(created_at: :desc)
     
     @sport_events = SportEvent.all
     
