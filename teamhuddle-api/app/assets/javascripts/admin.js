cbe510f83dbdb8613065d918988e1b9489ca71b5
Akos Sebestyen <akos_sebestyen@hotmail.com>
2014-12-01 00:06:54 -0800

    TH-97 search locations on creation and populate form

--- teamhuddle-api/app/assets/javascripts/admin.js
+++ teamhuddle-api/app/assets/javascripts/admin.js
@@ -1,6 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require angular
+//= require ngmap
 //= require adminApp
 
 
--- teamhuddle-api/app/assets/javascripts/adminApp.js
+++ teamhuddle-api/app/assets/javascripts/adminApp.js
@@ -1,22 +1,58 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngMap']);
 
 app.controller('import', ['$scope', '$http', function ($scope, $http) {
         $scope.result = "";
-        
+
         $scope.dropins = [];
-        
-        $scope.doScrape = function() {
-            
+
+        $scope.doScrape = function () {
+
             $http.post('scrape', {api_url: $scope.api_url}).
-                success(function (data, status, headers, config) {
-                    $scope.result = "it worked";
-                    $scope.dropins = data.dropins;
-                }).
-                error(function (data, status, headers, config) {
-                    // called asynchronously if an error occurs
-                    // or server returns response with an error status.
-                    $scope.result = "it DID NOT worked";
-                });
+                    success(function (data, status, headers, config) {
+                        $scope.result = "it worked";
+                        $scope.dropins = data.dropins;
+                    }).
+                    error(function (data, status, headers, config) {
+                        // called asynchronously if an error occurs
+                        // or server returns response with an error status.
+                        $scope.result = "it DID NOT worked";
+                    });
+        };
+    }]);
+
+app.controller('location-create', ['$scope', 'GeoCoder', function ($scope, GeoCoder) {
+        $scope.result = {};
+        
+        $scope.result.address = '';     
+        
+        var searchedFor = '';
+
+        $scope.search = function () {
+            searchedFor = $scope.searchInput;
+            var bounds = new google.maps.LatLngBounds(
+                new google.maps.LatLng(49.013143, -123.329683),
+                new google.maps.LatLng(49.470354, -122.016295)
+            );
+    
+            var before_search = $scope.result.address;
+            
+            GeoCoder.geocode({address: $scope.searchInput, bounds: bounds}).then(function (result) {                
+                $scope.result.address = result[0].formatted_address;
+                $scope.result.lat = result[0].geometry.location.k;
+                $scope.result.long = result[0].geometry.location.B;
+            });
+            
+            if($scope.result.address === before_search){
+                $scope.result.address = "couldn't find it";
+            }
+                
         };
         
+        $scope.populateForm = function () {
+            $scope.name = searchedFor;
+            $scope.lat = $scope.result.lat;
+            $scope.long = $scope.result.long;
+            $scope.address = $scope.result.address;
+            
+        };
     }]);
