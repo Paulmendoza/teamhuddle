eb86194753316c1258f02986f849c9f19c4e83f1
Akos Sebestyen <akos_sebestyen@hotmail.com>
2015-01-11 17:02:58 -0800

    robots.txt updated

--- teamhuddle-api/app/assets/javascripts/landingpageController.js
+++ teamhuddle-api/app/assets/javascripts/landingpageController.js
@@ -14,6 +14,9 @@ app.controller('landing-page', ['$scope', '$http', function ($scope, $http) {
                 $scope.success = true;
                 $scope.processing = false;
                 $scope.user = data;
+
+                // Google Analytics goal event
+                ga('send', 'event', 'newsletter-sign-up', 'email-submit', 'beta-newsletter-signup', 1);
             }).
             error(function (data, status, headers, config) {
                 $scope.success = false;
