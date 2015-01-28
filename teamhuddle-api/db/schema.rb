d626965770d49d75dd992b8c5f20e2e5201e498c
Akos Sebestyen <akos_sebestyen@hotmail.com>
2015-01-27 23:50:59 -0800

    TH-204 working on omniauth

--- teamhuddle-api/db/schema.rb
+++ teamhuddle-api/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150128071106) do
+ActiveRecord::Schema.define(version: 20150128074034) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -63,6 +63,16 @@ ActiveRecord::Schema.define(version: 20150128071106) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "identities", force: true do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "identities", ["user_id"], name: "index_identities_on_user_id"
 
   create_table "locations", force: true do |t|
     t.string   "address"
