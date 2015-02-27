10dc4815bb3a4affc7ca0d7a3b017eacb37683cf
Danny Tran <dannyhaitran@outlook.com>
2015-02-26 22:38:01 -0800

    initial commit regarding website visual overhaul 2

--- teamhuddle-api/app/assets/javascripts/base/baseApp.js
+++ teamhuddle-api/app/assets/javascripts/base/baseApp.js
@@ -15,16 +15,12 @@ window.addEventListener("scroll", function() {
     if (window.scrollY > 200) {
         if(faded){
             faded = false;
-            //$("nav").fadeTo(200, 1.0, "swing", function () {
-            //});
             $("nav").css("background-color", "white");
         }
     }
     else {
         if(!faded){
             faded = true;
-            //$( "nav" ).fadeTo( 200 , 0.2, "swing", function() {
-            //});
             $("nav").css("background-color", "transparent");
         }
 
