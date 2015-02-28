28a984764d7ea91db6c84591e5d3ece0299bc359
Akos Sebestyen <akos_sebestyen@hotmail.com>
2015-02-28 14:56:47 -0800

    deleting unneeded files and renaming

--- teamhuddle-api/config/initializers/assets.rb
+++ teamhuddle-api/config/initializers/assets.rb
@@ -3,11 +3,9 @@
 # and open the template in the editor.
 
 Rails.application.config.assets.precompile += %w( application.css )
-Rails.application.config.assets.precompile += %w( style.css )
+Rails.application.config.assets.precompile += %w( admin_style.css )
 Rails.application.config.assets.precompile += %w( admin/admin.js )
-Rails.application.config.assets.precompile += %w( animate.css )
 Rails.application.config.assets.precompile += %w( typography.css )
-Rails.application.config.assets.precompile += %w( static_pages.css )
 
 Rails.application.config.assets.precompile += %w( base_app.js )
 Rails.application.config.assets.precompile += %w( base_angular_app.js )
