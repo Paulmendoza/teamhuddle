6b50a2f70cac31c24b9e941b604b36037cdcd41c
Akos Sebestyen <akos_sebestyen@hotmail.com>
2015-04-04 12:17:08 -0700

    change date range for dropin email

--- teamhuddle-api/app/mailers/sport_event_notifier.rb
+++ teamhuddle-api/app/mailers/sport_event_notifier.rb
@@ -10,7 +10,7 @@ class SportEventNotifier < ActionMailer::Base
                                                                                 ORDER BY dt_expiry DESC
                                                                                 LIMIT 1)
                                             WHERE se.dt_expiry > ? AND se.dt_expiry < ?
-                                            ORDER BY dt_expiry ASC', Date.today - 1.weeks, Date.today + 2.weeks])
+                                            ORDER BY dt_expiry ASC', Date.today - 3.weeks, Date.today + 2.weeks])
 
     mail to: Rails.env.production? ? "eje.marc.daniel@gmail.com" : "akos_sebestyen@hotmail.com",
          cc: "akos_sebestyen@hotmail.com",
